6659b0c1a171619908fa737f6ab673c34357fd9c
Duy Nguyen <dnguyen9074@gmail.com>
2025-04-25 08:34:33 -0400

    init mantine/vite setup

---
+++ frontend/src/App.tsx
@@ -0,0 +1,16 @@
+import './styles/App.css'
+import '@mantine/core/styles.css';
+import { MantineProvider } from '@mantine/core';
+
+import { } from 'react-router-dom'
+
+function App() {
+
+  return (
+    <MantineProvider>
+      
+    </MantineProvider>
+  )
+}
+
+export default App
